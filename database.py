32375fa82514676a42fe12b8ff3fd097bf51f520
kvius <kvius2@ukr.net>
2024-02-29 08:43:04 +0200

    version 0.0.2

--- database.py
+++ database.py
@@ -39,16 +39,34 @@ class DatabaseManager:
             cursor.close()
             return None
 
+    def get_cur_date(self):
+        """Retrieves the current date from the MySQL database."""
+
+        self.connect()  # Ensure a database connection exists
+        query = "SELECT CURDATE()"
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(query)
+            result = cursor.fetchone()  # Fetch the date as a tuple
+            cursor.close()
+            return result[0]  # Return the date object (datetime.date)
+        except mysql.connector.Error as e:
+            print(f"MySQL Error: {e}")
+            cursor.close()
+            return None
+
     def check_credentials(self, username, password):
         self.connect()
-        query = "SELECT role FROM admins WHERE login = %s AND password = %s"
+        query = "SELECT role,`group` FROM admins WHERE login = %s AND password = %s"
         cursor = self.connection.cursor()
         try:
             cursor.execute(query, (username, password))
             result = cursor.fetchone()
             cursor.close()
             if result:
-                return {"role": result[0]}
+                print(result)
+                return {"role": result[0],"group":str(result[1])}
             else:
                 return None
         except mysql.connector.Error as e:
